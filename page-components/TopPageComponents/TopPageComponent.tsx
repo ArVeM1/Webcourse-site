dff5cfa12704a3916fc88669865eacec8ca186ad
Amir <dusenbaev101@gmail.com>
2022-05-16 15:30:25 +0400

    Сделал сортировку

---
+++ page-components/TopPageComponents/TopPageComponent.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Advantages, HhData, Htag, P, Sort, Tag } from '../../components';
+import { SortEnum } from '../../components/Sort/Sort.props';
+import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
+import { ProductModel } from '../../interfaces/product.interface';
+import { sortReducer } from './Sort.reducer';
+import styles from './TopPageComponent.module.css';
+
+interface TopPageComponentProps {
+  firstCategory: TopLevelCategory;
+  page: TopPageModel;
+  products: ProductModel[];
+}
+
+export const TopPageComponent: React.FC<TopPageComponentProps> = ({ page, products, firstCategory }) => {
+  const [{ products: sortedProducts, sort }, dispath] = React.useReducer(sortReducer, {
+    products,
+    sort: SortEnum.Rating,
+  });
+
+  const setSort = (sort: SortEnum) => {
+    dispath({ type: sort });
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <Sort sort={sort} setSort={setSort} />
+      </div>
+
+      <div>{sortedProducts && sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}</div>
+
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        {products && (
+          <Tag color="red" size="m">
+            hh.ru
+          </Tag>
+        )}
+      </div>
+
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+      {page.advantages && page.advantages.length > 0 && (
+        <>
+          <Htag tag="h2">Преимущества</Htag>
+          <Advantages advantages={page.advantages} />
+        </>
+      )}
+      {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+      <Htag tag="h2">Получаемые навыки</Htag>
+      {page.tags.map((t) => (
+        <Tag key={t} color="primary">
+          {t}
+        </Tag>
+      ))}
+    </div>
+  );
+};
